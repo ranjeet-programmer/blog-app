55f5ed2c2e9c7d44091ee40bb93a0e9a3b961f0b
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-03-01 18:16:39 +0530

    connected to the mongoDb

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ const app = express();
 
 const { PORT } = require("./config/serverConfig");
 
+const connectToDatabase = require("./config/database");
+
 const setupAndStart = () => {
   app.listen(PORT, () => {
     console.log(` server is started on port ${PORT} number 300`);
+
+    connectToDatabase();
   });
 };
 
